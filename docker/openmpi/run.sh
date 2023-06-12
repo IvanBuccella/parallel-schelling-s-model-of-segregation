aa0d51ea4f62d4a88ea1e9a7b8da1eb3702c769f
Ivan Buccella <ivan@buccella.me>
2023-06-12 23:31:32 +0200

    Created docker service with run script.

---
+++ docker/openmpi/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd /app
+
+echo "Compiling the project"
+mpicc main.c -o main.out
+echo "End compiling"
+
+echo "Starting the test"
+echo "Start test" > log.txt
+for i in {1..8}
+do
+  echo "Run with $i processors" >> log.txt
+  mpirun -np $i --allow-run-as-root ./main.out >> log.txt
+done
+echo "Test ended"
